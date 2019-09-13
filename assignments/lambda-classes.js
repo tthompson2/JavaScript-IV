f19798fa4777d73e205b3d052042c31179d1a831
Trevor Thompson <Thompson_Trevor@bah.com>
2019-09-13 16:01:39 -0400

    Coded classes as instructed for LC javascript file. I have yet to test it

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -14,6 +14,7 @@ class Person {
 
 class Instructor extends Person {
     constructor(instrutorAttributes) {
+        super(instrutorAttributes);
         this.speciality = instrutorAttributes.speciality;
         this.favLanguage = instrutorAttributes.favLanguage;
         this.catchPhrase = instrutorAttributes.catchPhrase;
@@ -27,5 +28,45 @@ class Instructor extends Person {
 
     grade(student, subject) {
         console.log(`${student.name} recieves a perfect score on ${subject}`);
+    }
+}
+
+class student extends Person {
+   constructor(studentAttributes) {
+       super(studentAttributes);
+       this.previousBackground = studentAttributes.previousBackground;
+       this.className = studentAttributes.className;
+       this.favSubjects = studentAttributes.favSubjects;
+   }
+
+   listSubjects() {
+       for(i = 0; i < this.favSubjects.length; i++)
+       {
+           console.log(this.favSubjects[i]);
+       }
+   }
+
+   PRAassignment(subject) {
+       console.log(`${student.name} has submitted a PR for ${subject}`);
+   }
+
+   sprintChallenge(subject) {
+       console.log(`${student.name} has begun sprint challenge on ${subject}`);
+   }
+}
+
+class projectManager extends Instructor {
+    constructor(pmAttributes) {
+        super(pmAttributes);
+        this.gradClassName = pmAttributes.gradClassName;
+        this.favInstructor = pmAttributes.favInstructor;
+    }
+
+    standUP(channel) {
+         console.log(`${this.gradClassName} announces to ${channel} @channel standby times`);
+    }
+
+    debugsCode() {
+        console.log(`${this.gradClassName} debugs ${student.name}'s code on ${subject}`);
     }
 }
